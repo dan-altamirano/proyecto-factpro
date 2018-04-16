0bece77adc4024cf08e9c84b310d4658ad772d1d
Dan Altamirano <dan.altamirano@alumnos.udg.mx>
2018-04-16 18:41:48 -0500

    Vaidacion de inicio de sesion

---
+++ js/actionsIn.js
@@ -0,0 +1,32 @@
+//Obtener los botones tipo submit
+let submitButton = $('button[type=submit]');
+//Agregar el listener para el submit
+submitButton.on('click',validateForm);
+
+var y = $("input");
+
+console.log(y);
+
+$.each(y, function() {
+  let element = $(this);
+
+  element.change(function(){
+      if(element.attr("class") == "inputc error")
+      {
+        element.removeClass("error");
+        let a = element.attr("name");
+        a = "span#" + a ;
+        $(a).hide();
+      }
+  });
+
+});
+
+/*
+$("#name").change(function(){
+          if($("#name").attr("class") == "inputc error")
+          {
+            $("#name").removeClass("error");
+          }
+});
+*/
