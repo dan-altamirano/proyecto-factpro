b50492b8d057810df81ada493a3cf244478e140e
Alex <Alex Rodriguez>
2018-04-16 23:50:48 -0500

    Adaptacion de nuevo formato responsivo a tabla page_admin_ventas

---
+++ js/valida.js
@@ -0,0 +1,155 @@
+let validateForm = function validateForm(e){
+  let $button = $(this);
+  let pristineR = true;
+  let pristineP = true;
+  let date = new Date();
+  //Obtain form
+  let $form = $button.parents('form');
+
+  //Obtatin required elements
+  let $required = $form.find('*[required]'); //Input / *
+  let $pattern = $form.find('*[pattern]');
+
+  pristineR = validateRequiredFields($required, date);
+  pristineP = validatePatternFields($pattern);
+
+  if(pristineR && pristineP){
+
+    if($form.attr("id") == "signup")
+    {
+      swal("¡Genial!", "¡Ahora eres miembro de FactPro!", "success")
+    }
+
+    if($form.attr("id") == "agregar")
+    {
+      swal("¡Bien!", "Agregado correctamente", "success")
+    }
+
+    if($form.attr("id") == "modificar")
+    {
+      swal("¡Bien!", "Modificado correctamente", "success")
+    }
+
+    if($form.attr("id") == "ctcform")
+    {
+      swal("¡Hemos recibido tu información!", "¡Gracias! En breve nos pondremos en contacto contigo", "success")
+    }
+
+  }
+
+  e.preventDefault(); //Don't execute
+
+}
+
+
+let getAge = function getAge(date, birth)
+{
+    var birthday = new Date(birth);
+    var age = date.getFullYear() - birthday.getFullYear();
+    var m = date.getMonth() - birthday.getMonth();
+
+    if (m < 0 || (m === 0 && date.getDate() < birthday.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+let validateRequiredFields = function validateRequiredFields(fields, date){
+    //console.log(fields);
+    let pristine = true;
+
+    $.each(fields, function() {
+      let element = $(this);
+
+
+      if(element.is('input') && element.val() === '')
+      {
+        generateError(element, 'es requerido', element.attr("name"));
+        pristine = false;
+      }
+
+      if(element.is('select') && element.val() === 0) //Marcar selected en 0 value
+      {
+        generateError(element, 'es requerido', element.attr("name"));
+        pristine = false;
+      }
+
+      if(element.is('input') && element.attr("type") == "date")
+      {
+        let age = getAge(date, element.val());
+
+        if(age < 18)
+        {
+          swal({
+            title: "¡Atención!",
+            text: "¡Para poder pertenecer a FactPro debes ser mayor de edad!",
+            icon: "warning",
+          })
+          pristine = false;
+        }
+
+      }
+    });
+
+    return pristine;
+}
+
+
+let validatePatternFields = function validatePatternFields(fields){
+    let pristine = true;
+    $.each(fields, function() {
+      let element = $(this);
+      let regex = element.attr('pattern');
+      if(!element.val().match(regex)){
+        generateError(element,'no va con el formato', element.attr("name"));
+        pristine = false;
+      }
+    });
+    return pristine;
+}
+
+
+
+let generateError = function generateError(element, typeError, typeInfo){
+
+  if(element.attr("class") != "inputc error")
+  {
+    element.addClass('error');
+    $('<span id = "' + typeInfo + '" class="errorMessage">Este campo ' + typeError + '</span>').insertAfter(element);
+  }
+}
+
+let doAnAction =  function doAnAction(e){
+  let $button = $(this);
+
+  if($button.attr("name") == "eliminar")
+  {
+    swal({
+    title: "¿Estas seguro?",
+    text: "¡Una vez eliminado, no podrás recuperar el registro!",
+    icon: "warning",
+    buttons: ["Cancelar", "Eliminar"],
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("¡El registro ha sido eliminado!", {
+        icon: "success",
+      });
+    } else {
+      swal("Eliminación Cancelada");
+    }
+  });
+  }
+
+  if($button.attr("name") == "finInventario")
+  {
+    swal("Inventario Terminado", "¡Información almacenada correctamente!", "success")
+  }
+
+  if($button.attr("name") == "editInventario")
+  {
+    swal("Inventario Modificado", "¡Información actualizada correctamente!", "success")
+  }
+
+}
